7c31cde2e895bb02ef94f5598b24a1df3bc17bcd
Bailee Manzer <baileemanzer3@gmail.com>
2019-11-08 16:47:31 -0700

    what the heck am i missing

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,21 +1,32 @@
 var topics = ["Koala Bear", "Dinosaur", "Giraffe"];
 
 function displayAnimalGif(){
-    var animals = $(this).attr("data-name");
+    var animal = $(this).attr("data-name");
+    console.log(animal);
     //Storing our giphy API URL for a random animal giph
-    var queryURL = "https://api.giphy.com/v1/gifs/random?api_key=1pDt2txaHIeBAvZSB9aasrgBlKM3Of6A&tag=&rating=G";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=1pDt2txaHIeBAvZSB9aasrgBlKM3Of6A&q=&limit=10&offset=0&rating=G&lang=en";
+    
+    console.log ('queryURL', queryURL);
+    
     // Perfoming an AJAX GET request to our queryURL for the specific animal GIF we want
     $.ajax({
-    url: queryURL,
-    method: "GET"
+        url: queryURL,
+        method: "GET",
     }).then(function(response){
         console.log(response);
         var results = response.data;
+        //Loop GIPHY results
         for (var i = 0; i < results.length; i++) {
+            //Creat newDiv to store the GIF
             var newDiv = $("<div>");
-            var gifBox = $("#gif-box");
-            var text
+            //Create image tag for GIF
+            var gifImage = $("<img>");
+          
+            gifDiv.append(gifImage);
+            $("#gifsGoHere").prepend(newDiv);
         }
     });
 }
+
+
    
