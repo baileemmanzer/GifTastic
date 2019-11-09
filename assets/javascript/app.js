1434fcc10ee4e9a06fd93c555af1a3430613642f
Bailee Manzer <baileemanzer3@gmail.com>
2019-11-08 17:34:39 -0700

    giraffe test/doesnt work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,10 +1,11 @@
-var topics = ["Koala Bear", "Dinosaur", "Giraffe"];
+var animal = "Giraffe"
+// var topics = ["Koala Bear", "Dinosaur", "Giraffe", "Cat", "Dog", "Chicken"];
 
 function displayAnimalGif(){
-    var animal = $(this).attr("data-name");
+    $(document.body).on('click', '.animal', displayAnimalGif)
     console.log(animal);
     //Storing our giphy API URL for a random animal giph
-    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=1pDt2txaHIeBAvZSB9aasrgBlKM3Of6A&q=&limit=10&offset=0&rating=G&lang=en";
+    var queryURL = "https://api.giphy.com/v1/gifs/search?q=" + animal + "&api_key=1pDt2txaHIeBAvZSB9aasrgBlKM3Of6A&limit=10&offset=0&rating=G&lang=en";
     
     console.log ('queryURL', queryURL);
     
@@ -13,20 +14,41 @@ function displayAnimalGif(){
         url: queryURL,
         method: "GET",
     }).then(function(response){
-        console.log(response);
+        // console.log(response);
         var results = response.data;
         //Loop GIPHY results
         for (var i = 0; i < results.length; i++) {
             //Creat newDiv to store the GIF
             var newDiv = $("<div>");
-            //Create image tag for GIF
-            var gifImage = $("<img>");
+            //Create image tag to hold GIF?
+            var gifImage = $("<img>").attr("src", queryURL);
           
-            gifDiv.append(gifImage);
+            //Append gifImage to the newDiv
+            newDiv.append(gifImage);
+           
             $("#gifsGoHere").prepend(newDiv);
         }
     });
 }
+
+function renderButtons (){
+    $("#buttons-view").empty();
+
+    // Looping through the array of movies
+    for (var i = 0; i < topics.length; i++) {
+
+      var a = $("<button>");
+      // Adding a class of gif-btn to our button
+      a.addClass("gif-btn");
+      // Adding a data-attribute
+      a.attr("data-name", topics[i]);
+      // Providing the text for button
+      a.text(topics[i]);
+      // Adding the button to the buttons-view div
+      $("#buttons-view").append(a);
+    }
+}
+
 
 
    
